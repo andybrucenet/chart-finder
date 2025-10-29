e3c0af08fd5c53954d23337c0355a62f93f8b69c
Andy Bruce <andybrucenet@gmail.com>
2025-10-29 09:20:23 -0400

    All scripting and configuration setup for serverless "sam" binary invocation

---
+++ scripts/aws-run-cmd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# aws-run-cmd.sh ABr
+#
+# Invoke a command with local AWS variables exported
+
+# locate script source directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+the_aws_run_cmd_script_dir="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+the_aws_run_cmd_root_dir="$( realpath "$the_aws_run_cmd_script_dir"/.. )"
+
+# source environment
+if [ -s "$the_aws_run_cmd_root_dir/.local/local.env" ] ; then
+  source "$the_aws_run_cmd_root_dir/.local/local.env" 'source-only' || exit $?
+fi
+#set | grep -e '^AWS_'
+
+# run cmd
+"$@"
+
--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -6,6 +6,10 @@
 ##############################################################
 # OPTIONS
 SETUP_DEV_ENV_OPTION_RESET_USER_SETTINGS="${SETUP_DEV_ENV_OPTION_RESET_USER_SETTINGS:-0}"
+SETUP_DEV_ENV_OPTION_RESET_SAM_CONFIG="${SETUP_DEV_ENV_OPTION_RESET_SAM_CONFIG:-0}"
+#
+# inferred values
+[ x"$SETUP_DEV_ENV_OPTION_RESET_USER_SETTINGS" = x1 ] && SETUP_DEV_ENV_OPTION_RESET_SAM_CONFIG=1
 
 # locate script source directory
 SOURCE="${BASH_SOURCE[0]}"
@@ -46,21 +50,30 @@ if [ -s "$the_setup_dev_env_local_env_path" ] ; then
 else
   # create it
   echo "#/bin/bash" > "$the_setup_dev_env_local_env_path"
+	echo "the_local_env_path_rc=0" >> "$the_setup_dev_env_local_env_path"
 fi
 #
 # vars
+if [ x"$AWS_PROFILE" = x ] ; then
+  export AWS_PROFILE=sab-u-dev
+  echo "[ x\"\$AWS_PROFILE\" = x ] && export AWS_PROFILE='$AWS_PROFILE'" >> "$the_setup_dev_env_local_env_path"
+fi
+if [ x"$AWS_SDK_LOAD_CONFIG" = x ] ; then
+  export AWS_SDK_LOAD_CONFIG=1
+  echo "[ x\"\$AWS_SDK_LOAD_CONFIG\" = x ] && export AWS_SDK_LOAD_CONFIG='$AWS_SDK_LOAD_CONFIG'" >> "$the_setup_dev_env_local_env_path"
+fi
 if [ x"$CF_LOCAL_DEV_ID" = x ] ; then
   the_default_value="$(whoami | sed -e 's/^l[\.\-]//')"
   read -p "  Enter value for CF_LOCAL_DEV_ID [$the_default_value]: " CF_LOCAL_DEV_ID
   CF_LOCAL_DEV_ID="${CF_LOCAL_DEV_ID:-$the_default_value}"
-  echo "export CF_LOCAL_DEV_ID='$CF_LOCAL_DEV_ID'" >> "$the_setup_dev_env_local_env_path"
+  echo "[ x\"\$CF_LOCAL_DEV_ID\" = x ] && export CF_LOCAL_DEV_ID='$CF_LOCAL_DEV_ID'" >> "$the_setup_dev_env_local_env_path"
   export CF_LOCAL_DEV_ID
 fi
 echo "  CF_LOCAL_DEV_ID='$CF_LOCAL_DEV_ID'"
 #
 if [ x"$CF_LOCAL_USEREMAIL" = x ] ; then
   read -p "  Enter value for CF_LOCAL_USEREMAIL: " CF_LOCAL_USEREMAIL
-  echo "export CF_LOCAL_USEREMAIL='$CF_LOCAL_USEREMAIL'" >> "$the_setup_dev_env_local_env_path"
+  echo "[ x\"\$CF_LOCAL_USEREMAIL\" = x ] && export CF_LOCAL_USEREMAIL='$CF_LOCAL_USEREMAIL'" >> "$the_setup_dev_env_local_env_path"
   export CF_LOCAL_USEREMAIL
 fi
 [ x"$CF_LOCAL_USEREMAIL" = x ] && echo 'Empty CF_LOCAL_USEREMAIL' && exit 1
@@ -70,7 +83,7 @@ if [ x"$CF_LOCAL_AWS_PROFILE" = x ] ; then
   the_default_value='sab-u-dev'
   read -p "  Enter value for CF_LOCAL_AWS_PROFILE [$the_default_value]: " CF_LOCAL_AWS_PROFILE
   CF_LOCAL_AWS_PROFILE="${CF_LOCAL_AWS_PROFILE:-$the_default_value}"
-  echo "export CF_LOCAL_AWS_PROFILE='$CF_LOCAL_AWS_PROFILE'" >> "$the_setup_dev_env_local_env_path"
+  echo "[ x\"\$CF_LOCAL_AWS_PROFILE\" = x ] && export CF_LOCAL_AWS_PROFILE='$CF_LOCAL_AWS_PROFILE'" >> "$the_setup_dev_env_local_env_path"
   export CF_LOCAL_AWS_PROFILE
 fi
 echo "  CF_LOCAL_AWS_PROFILE='$CF_LOCAL_AWS_PROFILE'"
@@ -79,10 +92,12 @@ if [ x"$CF_LOCAL_AWS_REGION" = x ] ; then
   the_default_value='us-east-2'
   read -p "  Enter value for CF_LOCAL_AWS_REGION [$the_default_value]: " CF_LOCAL_AWS_REGION
   CF_LOCAL_AWS_REGION="${CF_LOCAL_AWS_REGION:-$the_default_value}"
-  echo "export CF_LOCAL_AWS_REGION='$CF_LOCAL_AWS_REGION'" >> "$the_setup_dev_env_local_env_path"
+  echo "[ x\"\$CF_LOCAL_AWS_REGION\" = x ] && export CF_LOCAL_AWS_REGION='$CF_LOCAL_AWS_REGION'" >> "$the_setup_dev_env_local_env_path"
   export CF_LOCAL_AWS_REGION
 fi
 echo "  CF_LOCAL_AWS_REGION='$CF_LOCAL_AWS_REGION'"
+#
+echo "[ \$the_local_env_path_rc -eq 0 ] && true || false" >> "$the_setup_dev_env_local_env_path"
 #
 echo '  OK'
 echo ''
@@ -95,6 +110,7 @@ the_setup_dev_env_sam_config_local_fname='samconfig.local.toml'
 the_setup_dev_env_sam_config_base_path="$the_setup_dev_env_sam_config_dir/$the_setup_dev_env_sam_config_base_fname"
 the_setup_dev_env_sam_config_local_path="$the_setup_dev_env_sam_config_dir/$the_setup_dev_env_sam_config_local_fname"
 the_setup_dev_env_sam_config_work_path="${the_setup_dev_env_tmp_path_prefix}$the_setup_dev_env_sam_config_local_fname"
+[ x"$SETUP_DEV_ENV_OPTION_RESET_SAM_CONFIG" = x1 ] && rm -f "$the_setup_dev_env_sam_config_local_path"
 if [ -s "$the_setup_dev_env_sam_config_local_path" ] ; then
   echo "ALREADY_EXISTS: $the_setup_dev_env_sam_config_local_fname"
 else
@@ -114,7 +130,7 @@ else
   # initial setup creates a '.personal' section using defaults and non-interactive
   echo 'CREATE DEPLOYMENT...'
   set -x
-  sam deploy \
+  "$the_setup_dev_env_root_dir"/scripts/aws-run-cmd.sh sam deploy \
     --guided \
     --config-file "$the_setup_dev_env_sam_config_work_path" \
     --config-env dev \
